0ad0977d6f512dd6a623dbe95753712f44b63af8
sinowarden <sinowarden@gmail.com>
2025-10-07 16:32:56 +0800

    init gti two add new two

--- main.py
+++ main.py
@@ -17,4 +17,5 @@ if __name__ == '__main__':
     print("git push")
     print("git push test")
     print("git push test new")
+    print("git push test next")
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
